b954d8bd16e38078b4e1f9bd9460839409f39310
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-10-25 17:49:33 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let tasks = [];
+  const form = document.getElementById("create-task-form");
+  const description = document.getElementById("new-task-description");
+  const todos = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    todos.innerHTML += description.value;
+    event.preventDefault();
+    }, false);
+
+  });
